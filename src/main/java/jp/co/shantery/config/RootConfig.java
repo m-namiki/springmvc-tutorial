80a1a0633efd6eb647c24fb33b22f26070bbd012
m-namiki <masa1824@gmail.com>
2013-03-07 15:32:13 +0900

    init

---
+++ src/main/java/jp/co/shantery/config/RootConfig.java
@@ -0,0 +1,18 @@
+package jp.co.shantery.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@ComponentScan(basePackages = { "jp.co.shantery" })
+public class RootConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		return ppc;
+	}
+	
+}

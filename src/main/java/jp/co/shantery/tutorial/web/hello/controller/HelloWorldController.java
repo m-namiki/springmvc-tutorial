aad6562e388fdd9e9ca2f839a520366d9fd972ac
m-namiki <masa1824@gmail.com>
2013-03-08 13:44:31 +0900

    add hello world. delete other functions.

---
+++ src/main/java/jp/co/shantery/tutorial/web/hello/controller/HelloWorldController.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package jp.co.shantery.tutorial.web.hello.controller;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * HelloWorldを表示するコントローラです。
+ * 
+ * @author m-namiki
+ * 
+ */
+@Controller
+public class HelloWorldController {
+
+	private static Logger logger = Logger.getLogger(HelloWorldController.class);
+
+	/**
+	 * hello.jspを表示します。
+	 * 
+	 * @return hello.jspのパス
+	 */
+	@RequestMapping("/hello")
+	public ModelAndView index() {
+		logger.info("index() Start.");
+
+		ModelAndView view = new ModelAndView();
+		view.setViewName("hello/hello");
+		view.addObject("message", "Hello Spring MVC world.");
+		view.addObject("currentDate", new Date());
+
+		logger.info("index() End.");
+		return view;
+	}
+
+}
